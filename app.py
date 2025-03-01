932407b0c0977ad749d24001b3cb0cbbf3dbc16f
Tanikai-Ganesh <tanikaiceg02@gmail.com>
2025-03-01 06:45:52 +0530

    Adding Docker File

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def predict_datapoint():
     return jsonify({'error': 'Method not allowed'}), 405
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080,debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
